9b9b28235eb7f511cb29d00a73a3e109f08b8446
x-treme <yurchenko-inform@yandex.ru>
2024-06-26 17:31:33 +0300

    change main.js

--- main.js
+++ main.js
@@ -2,17 +2,23 @@
  * создать JSON Formatter. Но результат показываем не по нажатию кнопки, 
  * а проверяем каждые 3 секунды, что ввел пользователь и если там корректный JSON-текст, 
  * справа выводим его в отформатированном виде
+ * 
+ * {"objAskGrant":"Yes","objPass":"asdfasdf","objNameSurname":"asdfasdf adfasdf",
+ * "objEmail":"asdfaf@asdfaf.com","objGsm":3241234123,"objAdres":"asdfasdf",
+ * "objTerms":"CheckIsValid"}
  */
 
 let inputJson = document.getElementById('inputJson').value;
 
+
 setInterval(formattedJson, 3000, inputJson);
 
-function formattedJson($inputJson){
+function formattedJson(inputJson){
     let object = '';
-
-     try {
-        object = JSON.parse($inputJson); // Получили json-строку, превратили ее в объект и сразу проверили на ошибки через try-catch
+    try {
+        object = JSON.parse(inputJson); // Получили json-строку, превратили ее в объект и сразу проверили на ошибки через try-catch
+        //console.log(object);
+        
         let formattedJson = JSON.stringify(object, null, 4); // если ошибок нет, то отформатируем 
         document.getElementById('outputDiv').innerHTML = formattedJson;
 
@@ -20,6 +26,7 @@ function formattedJson($inputJson){
         console.error("При разборе JSON произошла ошибка:", e); //проблемы с JSON!
         document.getElementById('message').innerHTML = '[format error!]';
       }
+
 }
 
 
